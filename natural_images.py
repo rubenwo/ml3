45e4f1e49fcf10c8985cd261764c5ac2c7ae7a2e
Ruben <rwoldhui@avans.nl>
2020-09-30 14:46:39 +0200

    [Fixed] loading data

--- natural_images.py
+++ natural_images.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 
 from dataprep import get_data
 
-data_set = get_data("./data/natural_images")
+train_ds, val_ds = get_data()
 
 num_filters = 8
 filter_size = 3
@@ -11,7 +11,7 @@ pool_size = 2
 
 # Build the model.
 model = tf.keras.Sequential([
-    Conv2D(num_filters, filter_size, input_shape=(256, 256, 1)),
+    Conv2D(num_filters, filter_size, input_shape=(256, 256, 3)),
     MaxPooling2D(pool_size=pool_size),
     Flatten(),
     Dense(10, activation='softmax'),
@@ -19,12 +19,13 @@ model = tf.keras.Sequential([
 # Compile the model.
 model.compile(
     'adam',
-    loss='categorical_crossentropy',
+    loss='sparse_categorical_crossentropy',
     metrics=['accuracy'],
 )
 
 # Train the model.
 model.fit(
-    data_set,
+    train_ds,
+    validation_data=val_ds,
     epochs=3,
 )
