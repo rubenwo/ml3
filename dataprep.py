65eef70637b52a2f91ee9b18eda8fa346218d9cf
Ruben <rwoldhui@avans.nl>
2020-10-07 17:20:44 +0200

    [Changed] predictions to 'categorical'
    which in this case is a vector of size 8

--- dataprep.py
+++ dataprep.py
@@ -4,18 +4,24 @@ import tensorflow as tf
 def get_data(validation_split, image_size=(256, 256)):
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
         "./data/natural_images",
+        labels='inferred',
+        label_mode="categorical",
         validation_split=validation_split,
         subset="training",
         seed=123,
         image_size=image_size,
+        color_mode='rgb',
         batch_size=32)
 
     val_ds = tf.keras.preprocessing.image_dataset_from_directory(
         "./data/natural_images",
+        labels='inferred',
+        label_mode="categorical",
         validation_split=validation_split,
         subset="validation",
         seed=123,
         image_size=image_size,
+        color_mode='rgb',
         batch_size=32)
 
     train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
